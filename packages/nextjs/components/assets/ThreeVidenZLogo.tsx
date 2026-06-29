020dd51bf81e375dbf07e50c120c22b7f2e571c9
BCD <vincent.langard@gmail.com>
2026-06-30 00:12:39 +0200

    fix: move eslint-disable comment to correct line in ThreeVidenZLogo

--- packages/nextjs/components/assets/ThreeVidenZLogo.tsx
+++ packages/nextjs/components/assets/ThreeVidenZLogo.tsx
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @next/next/no-img-element
 export const ThreeVidenZLogo = ({ className }: { className: string }) => {
+  // eslint-disable-next-line @next/next/no-img-element
   return <img src="/Ǝvidenz.png" alt="3videnZ" className={className} />;
 };
